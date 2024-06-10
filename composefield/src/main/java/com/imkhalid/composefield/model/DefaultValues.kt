ac1ff7003e8d868eb769a8d0c68dfbf59077cfee
Khalid Saeed <khalid.saeed@ozoneddigital.com>
2024-06-10 11:58:55 +0500

    Made DefaultValues Parcelable as they cause crash when navigation to other apps.

--- composefield/src/main/java/com/imkhalid/composefield/model/DefaultValues.kt
+++ composefield/src/main/java/com/imkhalid/composefield/model/DefaultValues.kt
@@ -1,5 +1,8 @@
 package com.imkhalid.composefield.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 
 data class DefaultValues(
     val id: String,
@@ -7,4 +10,35 @@ data class DefaultValues(
     val code: String? = null,
     val form_fields_id: String? = null,
     var isChecked: Boolean = false
-)
+):Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()?:"",
+        parcel.readString()?:"",
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readByte() != 0.toByte()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(text)
+        parcel.writeString(code)
+        parcel.writeString(form_fields_id)
+        parcel.writeByte(if (isChecked) 1 else 0)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<DefaultValues> {
+        override fun createFromParcel(parcel: Parcel): DefaultValues {
+            return DefaultValues(parcel)
+        }
+
+        override fun newArray(size: Int): Array<DefaultValues?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
