b43be0d3e67a55ae6fdfbdd1efbe244df4a717c4
Khalid Saeed <khalid.saeed@ozoneddigital.com>
2024-06-20 10:09:26 +0500

    Added Changes Related apis for onboarding.

--- composefield/build.gradle.kts
+++ composefield/build.gradle.kts
@@ -40,65 +40,63 @@ android {
     }
 }
 
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("mavenJava") {
-                from(components["release"])
-
-                groupId = "com.github.imkhalid"
-                artifactId = "ComposeField"
-                version = "1.0.15" // You should dynamically set this or match your actual versioning
-
-                // Configure the POM file
-                pom {
-                    name.set("ComposeField")
-                    description.set("A Library to create all type of Fields base on Module supplied")
-                    url.set("https://github.com/imkhalid/ComposeField")
-
-                    scm {
-                        connection.set("scm:git:git://github.com/imkhalid/ComposeField.git")
-                        developerConnection.set("scm:git:ssh://github.com:imkhalid/ComposeField.git")
-                        url.set("https://github.com/imkhalid/ComposeField")
-                    }
-
-                    licenses {
-                        license {
-                            name.set("The Apache License, Version 2.0")
-                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                        }
-                    }
-
-                    developers {
-                        developer {
-                            id.set("imkhalid")
-                            name.set("Khalid Saeed")
-                            email.set("your_email@example.com")
-                        }
-                    }
-
-                    withXml {
-                        val dependenciesNode = asNode().appendNode("dependencies")
-                        configurations["implementation"].allDependencies.forEach {
-                            if (it.group != null && it.version != null) {
-                                val dependencyNode = dependenciesNode.appendNode("dependency")
-                                dependencyNode.appendNode("groupId", it.group)
-                                dependencyNode.appendNode("artifactId", it.name)
-                                dependencyNode.appendNode("version", it.version)
-                                dependencyNode.appendNode("scope", "compile")
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        repositories {
-            mavenLocal()
-        }
-    }
-
-
-}
+//afterEvaluate {
+//    publishing {
+//        publications {
+//            create<MavenPublication>("mavenJava") {
+//                from(components["release"])
+//
+//                groupId = "com.github.imkhalid"
+//                artifactId = "ComposeField"
+//                version = "1.0.15" // You should dynamically set this or match your actual versioning
+//
+//                // Configure the POM file
+//                pom {
+//                    name.set("ComposeField")
+//                    description.set("A Library to create all type of Fields base on Module supplied")
+//                    url.set("https://github.com/imkhalid/ComposeField")
+//
+//                    scm {
+//                        connection.set("scm:git:git://github.com/imkhalid/ComposeField.git")
+//                        developerConnection.set("scm:git:ssh://github.com:imkhalid/ComposeField.git")
+//                        url.set("https://github.com/imkhalid/ComposeField")
+//                    }
+//
+//                    licenses {
+//                        license {
+//                            name.set("The Apache License, Version 2.0")
+//                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+//                        }
+//                    }
+//
+//                    developers {
+//                        developer {
+//                            id.set("imkhalid")
+//                            name.set("Khalid Saeed")
+//                            email.set("your_email@example.com")
+//                        }
+//                    }
+//
+//                    withXml {
+//                        val dependenciesNode = asNode().appendNode("dependencies")
+//                        configurations["implementation"].allDependencies.forEach {
+//                            if (it.group != null && it.version != null) {
+//                                val dependencyNode = dependenciesNode.appendNode("dependency")
+//                                dependencyNode.appendNode("groupId", it.group)
+//                                dependencyNode.appendNode("artifactId", it.name)
+//                                dependencyNode.appendNode("version", it.version)
+//                                dependencyNode.appendNode("scope", "compile")
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        repositories {
+//            mavenLocal()
+//        }
+//    }
+//}
 
 
 dependencies {
@@ -116,7 +114,7 @@ dependencies {
     api("androidx.compose.ui:ui-graphics")
     api("androidx.compose.ui:ui-tooling-preview")
     api("androidx.compose.material3:material3")
-    implementation(project(":library"))
+    api(project(":library"))
 
     implementation("androidx.hilt:hilt-navigation-compose:$hiltWork")
 
