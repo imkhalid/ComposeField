b6e20b7e4d56336c208313320e37861e53857e49
Khalid Saeed <khalid.saeed@ozoneddigital.com>
2024-08-21 17:16:10 +0300

    updated app versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,9 +56,9 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2024.04.01"))
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.activity:activity-compose:1.9.1")
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -67,7 +67,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.01"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
@@ -79,6 +79,6 @@ dependencies {
     kapt ("androidx.hilt:hilt-compiler:$hiltWork")
     implementation("androidx.hilt:hilt-navigation-compose:$hiltWork")
 
-    implementation("com.squareup.okhttp3:okhttp:4.11.0") // Use the latest version
+    implementation("com.squareup.okhttp3:okhttp:4.12.0") // Use the latest version
     implementation("com.squareup.retrofit2:converter-gson:2.9.0") // Use the latest version
 }
