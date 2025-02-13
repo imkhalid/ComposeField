a5a0b17ea4c45d03114bd06474f99d9a81ef78ce
Khalid Saeed <khalid.saeed@ozoneddigital.com>
2025-02-13 18:24:04 +0500

    Added TableConfig and moved table properties to it.

--- composefield/src/main/java/com/imkhalid/composefield/composeField/TableColors.kt
+++ composefield/src/main/java/com/imkhalid/composefield/composeField/TableColors.kt
@@ -1,6 +1,17 @@
 package com.imkhalid.composefield.composeField
 
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+
+data class TableConfig(
+    val tableColors: TableColors = TableColors(),
+    val showTitle: Boolean = false,
+    val showLimit: Boolean = false,
+    val showDescription:Boolean = false,
+    val tableAddButton: @Composable (ColumnScope.(onClick: () -> Unit) -> Unit)? = null,
+    val tablePopupButton: @Composable (ColumnScope.(onClick: () -> Unit, data:HashMap<String,List<ComposeFieldStateHolder>>) -> Unit)?=null,
+)
 
 data class TableColors(
     val sectionTitleColor: Color = Color.Black,
