506160001b87c43e4ad8b0d71c0ea22723890a73
Khalid Saeed <khalid.saeed@ozoneddigital.com>
2025-02-13 15:30:54 +0500

    Added fields is_table, min_rows, and max_rows to the section data class within CustomFormData.

--- composefield/src/main/java/com/imkhalid/composefield/model/CustomFormData.kt
+++ composefield/src/main/java/com/imkhalid/composefield/model/CustomFormData.kt
@@ -12,6 +12,9 @@ data class FormData(val risk_sections: Map<String, section>)
 data class section(
     val category_id: Int,
     val section_id: Int,
+    val is_table:Int,
+    val min_rows:Int,
+    val max_rows:Int,
     val sorting_number: Int,
     var custom_fields: List<CustomFields>,
     val section_name: String = "",
